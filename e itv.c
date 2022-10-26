b3841262d02156e4fde4ecfcba38b3c3b6fed1d5
Samuel <115562738+samuelrubio35@users.noreply.github.com>
2022-10-26 13:51:16 +0200

    Update e itv.c

--- e itv.c
+++ e itv.c
@@ -4,41 +4,67 @@
 
 int main()
 {
-    int coche, moto, especiales, peso, cilindrada;
-    int agricola, quad;
+    float peso, cilindrada;
 
-    printf("Vehiculo que utiliza hoy en dia (coche, moto o especial): ");
-    scanf("%i", &coche);
+    char tipo_v;
+        printf("Introduzca los datos tipo/peso/cilindrada: ");
+        scanf("%c/%f/%f",&tipo_v,&peso,&cilindrada);
 
-    printf("Peso del vehiculo: ");
-    scanf("%i", &peso);
 
-    if(peso<2000){
-       printf("Linea 1");
-    }else if (2000<=peso && peso<2500){
+if ((tipo_v == 'c') || (tipo_v == 'C'))
 
-    printf("Cilindrada: "); scanf("%d", &cilindrada);
-    if (cilindrada<1200)
-        printf("Linea 2");
-    else if (cilindrada>=1200) {
-        printf("Linea 5");
+    //OPCION COCHE
+
+    {
+
+      if (peso < 2000)
+
+        printf("Linea 1.\n");
+
+      else if ((peso >= 2000) && (peso < 2500))
+
+      {
+
+        if (cilindrada < 1200)
+
+          printf("Linea 3.\n");
+
+        else
+
+          printf("Linea 5.\n");
+
+      }
+
+      else if (peso >= 2500)
+
+        printf("Linea 2.\n");
 
     }
 
+  else if ((tipo_v == 'm') || (tipo_v == 'M'))
 
+    //OPCION MOTO
 
+    printf("Linea 2.\n");
 
+  else if ((tipo_v == 'q') || (tipo_v == 'Q'))
 
+    //OPCION QUAD
 
+    printf("Linea 4.\n");
 
+  else if ((tipo_v == 'a') || (tipo_v == 'A'))
 
+    //OPCION AGRICOLA
 
+    printf("Linea 1.\n");
 
+  else
 
-
-
+    printf("Tipo incorrecto o no soportado.\n");
 
 
 
     return 0;
+
 }
